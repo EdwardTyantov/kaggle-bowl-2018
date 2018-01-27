016c4e56c3ac595c8200fc95f9a01156bb0d5d32
Sergey Cheparukhin <chipa312@gmail.com>
2018-01-27 09:05:11 +0000

    Add connectivity param for morphology labelling

--- cell/utils.py
+++ cell/utils.py
@@ -92,10 +92,10 @@ def rle_encoding(x):
     return run_lengths
 
 
-def prob_to_rles(x, use_adaptive_threshold = False, cut_off = 0.5):
+def prob_to_rles(x, use_adaptive_threshold = False, cut_off = 0.5, connectivity = 2):
     if adaptive_threshold:
         cut_off = threshold_otsu(x)
-    lab_img = label(x>cut_off)
+    lab_img = label(x>cut_off, connectivity=connectivity)
     if lab_img.max()<1:
         lab_img[0,0] = 1 # ensure at least one prediction per image
     for i in range(1, lab_img.max()+1):
